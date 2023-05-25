2a34811de3b275c6f42c80fb6cac9b4ca8b3a0a5
Steven <level2fast@gmail.com>
2023-05-24 21:12:40 -0700

    Added new enemy assets
    Added new enemies and create tag for object that are spawned at runtime

--- BombSquad/Assets/Scripts/Enemy.cs
+++ BombSquad/Assets/Scripts/Enemy.cs
@@ -4,14 +4,21 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVfx;
     [SerializeField] GameObject hitVfx;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 10;
     [SerializeField] int hitPoints = 2;
     ScoreBoard scoreBoard;
-
+    Rigidbody _rigBody;
+    GameObject _parentGameObject;
     private void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        _parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddRigidBody();
+    }
+    private void AddRigidBody()
+    {
+        _rigBody = gameObject.AddComponent<Rigidbody>();
+        _rigBody.useGravity = false;
     }
     private void OnParticleCollision(GameObject other)
     {
@@ -24,14 +31,14 @@ public class Enemy : MonoBehaviour
     private void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVfx, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = _parentGameObject.transform;
         hitPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
     }
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVfx, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = _parentGameObject.transform;
         Destroy(gameObject);
     }
 }
